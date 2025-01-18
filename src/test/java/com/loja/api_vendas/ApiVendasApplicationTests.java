e4c14ead11dfb890e3955b02e1ea03f955f4e99c
Fran <francinette.borges@gmail.com>
2025-01-18 19:53:57 -0300

    [build]: Iniciando projeto

---
+++ src/test/java/com/loja/api_vendas/ApiVendasApplicationTests.java
@@ -0,0 +1,10 @@
+package com.loja.api_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasApplicationTests {
+
+
+}
